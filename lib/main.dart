223a6ca1e19f19db5cb47fc5cd9829858811520a
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-07 14:05:40 +0430

    the image is add and i add same change in code

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,20 @@ import 'start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(body: Center(child: StartScreen())),
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
     ),
   );
 }
