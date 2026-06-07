a15d810256b6034af9520a9e4c742099261e8173
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-07 14:17:05 +0430

    i add the image text button in screen page

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,28 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Center(child: const Text('Start Screen!'));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
