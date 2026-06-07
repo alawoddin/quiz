0b6659f94ea93a8fc062480b7ee22eb382041a8c
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-07 16:15:51 +0430

    i add the icon button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,10 +9,13 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 200,
-            color: const Color.fromARGB(150, 255, 255, 255),
+          Opacity(
+            opacity: 0.6,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 200,
+              color: const Color.fromARGB(150, 255, 255, 255),
+            ),
           ),
           const SizedBox(height: 80),
           const Text(
@@ -20,10 +23,11 @@ class StartScreen extends StatelessWidget {
             style: TextStyle(color: Colors.white, fontSize: 24),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: const Text('Start Quiz'),
+            icon: Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
           ),
         ],
       ),
