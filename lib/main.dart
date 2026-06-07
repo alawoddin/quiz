22428a3bd1470524a65b5650d962b8c256009b74
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-07 13:56:51 +0430

    the start screen page is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'start_screen.dart';
+
 void main() {
-  runApp(MaterialApp(
-    home:
-  ));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(body: Center(child: StartScreen())),
+    ),
+  );
 }
