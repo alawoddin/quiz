d3a00668192f04e3a63c17079e80a62f6a34afd5
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-10 15:07:06 +0430

    the answer section is add

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +13,23 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return Column(children: [Text()]);
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("text", style: TextStyle(color: Colors.white)),
+          const SizedBox(height: 30),
+
+          AnswerButton(answerText: 'Answer the Text 1', onTap: () {}),
+          const SizedBox(height: 10),
+
+          AnswerButton(answerText: 'Answer the Text 2', onTap: () {}),
+          const SizedBox(height: 10),
+
+          AnswerButton(answerText: 'Answer the Text 3', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
